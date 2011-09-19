7ff105feccae87fb6a6ec9bafa9daf19ec4c2db8
Leon Hu <tihuinc@gmail.com>
2011-09-18 23:06:20 -0700

    Revert "Revert "Changing the way that Rails 3.1 that requires using different syntax of using hash""
    This reverts commit 1be7b54844c668c8c56d75efff496f9bc5de6ffd.

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -6,7 +6,7 @@ class GymsController < ApplicationController
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @gyms }
+      format.json { render :json => @gyms }
     end
   end
 
@@ -17,7 +17,7 @@ class GymsController < ApplicationController
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @gym }
+      format.json { render :json => @gym }
     end
   end
 
@@ -28,7 +28,7 @@ class GymsController < ApplicationController
 
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render json: @gym }
+      format.json { render :json => @gym }
     end
   end
 
@@ -44,11 +44,11 @@ class GymsController < ApplicationController
 
     respond_to do |format|
       if @gym.save
-        format.html { redirect_to @gym, notice: 'Gym was successfully created.' }
-        format.json { render json: @gym, status: :created, location: @gym }
+        format.html { redirect_to @gym, :notice => 'Gym was successfully created.' }
+        format.json { render :json => @gym, :status => :created, :location => @gym }
       else
-        format.html { render action: "new" }
-        format.json { render json: @gym.errors, status: :unprocessable_entity }
+        format.html { render :action => "new" }
+        format.json { render :json => @gym.errors, :status => :unprocessable_entity }
       end
     end
   end
@@ -60,11 +60,11 @@ class GymsController < ApplicationController
 
     respond_to do |format|
       if @gym.update_attributes(params[:gym])
-        format.html { redirect_to @gym, notice: 'Gym was successfully updated.' }
+        format.html { redirect_to @gym, :notice => 'Gym was successfully updated.' }
         format.json { head :ok }
       else
-        format.html { render action: "edit" }
-        format.json { render json: @gym.errors, status: :unprocessable_entity }
+        format.html { render :action => "edit" }
+        format.json { render :json => @gym.errors, :status => :unprocessable_entity }
       end
     end
   end
