73c5e7120555ed2acbdefc6ae29345ea0417d47f
Alex Egg <eggie5@gmail.com>
2011-09-20 16:41:37 -0700

    updated fb keys

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -198,7 +198,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
-  config.omniauth :facebook, "150231875070757", "c98716fffaff3acae551c10102624b1d"
+  config.omniauth :facebook, "150346335059375", "c5f91b671a04cdec5e2b4d7c93c8bc39"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
