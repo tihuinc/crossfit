283446e38a089efb7fad495940a8e994d18fb921
Leon Hu <tihuinc@gmail.com>
2011-09-21 21:40:23 -0700

    Creates associations between users, words, and user_workouts

--- app/models/wod.rb
+++ app/models/wod.rb
@@ -1,5 +1,9 @@
 class Wod < ActiveRecord::Base
+  # constants
   TIME = "time".freeze
   AMRAP = "AMRAP".freeze
   WOD_TYPES = [TIME, AMRAP]
+
+  # associations
+  has_many :workout_records
 end
--- app/models/workout_record.rb
+++ app/models/workout_record.rb
@@ -1,2 +1,4 @@
 class WorkoutRecord < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :wod
 end
